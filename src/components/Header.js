8599a099a83ebfa38853e9e21785565536ccf52e
Riley <riley.s2003@hotmail.com>
2026-06-02 14:28:15 -0500

    added header title and navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,13 @@
+import Nav from './Nav';
+
 function Header() {
     return (
-        <></>
+        <header>
+            <div className="flex">
+                <h1>Components</h1>
+                <Nav/>
+            </div>
+        </header>
     );
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+function Nav() {
+    return (
+        <nav>
+            <ul className="flex">
+                <li><a href='#'>Home</a></li>
+                <li><a href='#'>About</a></li>
+                <li><a href='#'>Help</a></li>
+                <li><a href='#'>Contact</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
